4a64ad7f7736615688a5ffd66e7ec593a2d389a9
AlanDAmario <vega93@live.it>
2024-04-29 20:17:20 +0200

    [UPD] fix

--- js/main.js
+++ js/main.js
@@ -1,54 +1,16 @@
 "use strict";
-// variabile età//
-
-// const travelerAge = Number(('inserire l\' eta'));
-// console.log('età passeggero', travelerAge);
-
-// //variabile km da percorerre//
-
-// const travelerKM = Number(('inserire km da percorrere'));
-// console.log('travelKM', travelerKM);
-
-// //variabile prezzo per km//
-
-// const priceKM = 0.21;
-// console.log('priceKM', priceKM);
-
-// //PERCENTUALI//
-
-// const discount20 = 20;
-// console.log('sconto minorenni :' + discount20);
-
-// const discount40 = 40;
-// console.log('sconto over 65 :' + discount40);
-
-// //svolgimento//
-
-// let total = priceKM * travelerKM;
-
-// // }
-
-// if (travelerAge < 18) {
-//     total = (total / 100 * discount20);
-
-// } else if (travelerAge > 65) {
-//     total = (total / 100 * discount40);
-
-// }
-// //ARROTONDAMENTO DECIMALE//
-
-// console.log(`prezzo finale è' ${total.toFixed(2)}€`);
-
-
 // REFS FORM E INPUT
-const form = document.getElementById('btn-generator');
+const generatorBtn = document.getElementById('btn-generator');
 // console.log(form);
+//DELETE BTN//
+const deleteBtn = document.getElementById('btn-delete')
+// console.log(deleteBtn)
+const userName = document.querySelector('.user-name')
 const inputKm = document.querySelector('.km');
-// console.log(inputKm);
 const inputAge = document.querySelector('.age');
 const totalElement = document.getElementById('total');
 
-form.addEventListener('submit', function (e) {
+generatorBtn.addEventListener('click', function (e) {
     e.preventDefault();
     // lettra val;
     // //variabile km da percorerre//
@@ -75,6 +37,15 @@ form.addEventListener('submit', function (e) {
 
     // END EVENT FUNCTION
 })
+
+// DELETE VALUE//
+deleteBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    userName.value = ('');
+    inputKm.value = ('');
+    inputAge.value = ('');
+})
+
 
 // DETECT EVENT (submit oppure click)
 // LETTURA DEI VALORI INPUT
